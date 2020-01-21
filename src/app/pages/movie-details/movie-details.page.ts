92f72292e80d7ee329ff2b294656e705c74c615d
Lazar Slavkovic-Raco <slavkovic.lazar96@gmail.com>
2020-01-21 20:45:28 +0100

    Finished

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {MovieService} from '../../services/movie.service';
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.page.html',
-  styleUrls: ['./movie-details.page.scss'],
+    selector: 'app-movie-details',
+    templateUrl: './movie-details.page.html',
+    styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+    information = null;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private movieService: MovieService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        let id = this.activatedRoute.snapshot.paramMap.get('id');
 
+        this.movieService.getDetails(id).subscribe(result => {
+            console.log('details: ', result);
+            this.information = result;
+        });
+    }
+
+    openWebsite() {
+        window.open(this.information.Website, '_blank');
+    }
 }
